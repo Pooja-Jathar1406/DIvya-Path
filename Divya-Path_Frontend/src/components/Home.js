866bd8a1bd544c39a2320e47f22472b0a1acddc0
Pooja-Jathar1406 <jatharpooja1406@gmail.com>
2023-02-27 16:42:11 +0530

    new component added

--- Divya-Path_Frontend/src/components/Home.js
+++ Divya-Path_Frontend/src/components/Home.js
@@ -24,9 +24,8 @@ export class Home extends Component {
               </div>
             </div>
           </Container>
-          <Container>body content here</Container>
-          <div className="container">
-            <div className="row">
+          <div className="container ">
+            <div className="row ">
               <div className="col">
                 <div className="card navCards  " style={{ width: "auto" }}>
                   <div className="card-body">
@@ -64,7 +63,7 @@ export class Home extends Component {
               <div className="col">
                 <a href="./Header.html" className="custom-card">
                   <div
-                    className="card navCards activeNavCard text-center pt-3"
+                    className="card navCards   text-center pt-3"
                     style={{ width: "13rem", display: "inline-block" }}>
                     <img
                       src="../Images/Places.png"
@@ -102,7 +101,7 @@ export class Home extends Component {
                 </a>
               </div>
               <div className="col">
-                <a href="./Header.html" className="custom-card">
+                <a href="login" className="custom-card">
                   <div
                     className="card navCards text-center pt-3"
                     style={{ width: "12rem", display: "inline-block" }}>
