d31358d7a8ce3d8d81d1b2083c45f6a724a985f0
Harshal <harshaldeore05@gmail.com>
2023-03-05 23:55:28 +0530

    05/03/22 23:55

--- Divya-Path_Frontend/src/components/AddCourse.js
+++ Divya-Path_Frontend/src/components/AddCourse.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { Component, useState } from "react";
 import { Container } from "react-bootstrap";
 import "../css/Home.css";
 import { Navigation } from "./Navigation";
@@ -8,22 +8,37 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import './formcss.css';
 export function AddCourse() {
+  const initialValues = {coursename: ""}; //field to be filled
+  const [coursedetails,setcoursedetails]= useState(initialValues);
+  const handleChange = (e) => {
+    setcoursedetails({ ...coursedetails, [e.target.name]:e.target.value });
+  };
+  const onSubmit= async(e) => {
+    e.preventDefault();
+    const response = await.save(coursedetails) ///enter a name of method name from backend
+    console.log(response.data); //to check if data is entered or not
+  };
+
   return (
     <>
      <Navigation></Navigation>
     <div className="div1">
       <h4>Add Course</h4>
       <br></br>
-      <Form className="formcss">
+      <Form className="formcss" onSubmit={handleSubmit}>
       <Form.Group>
           <Form.Label>Enter Course Name:</Form.Label>
           <Form.Control type="text" 
-                        placeholder="Enter course name" />
+                        placeholder="Enter course name" 
+                        values={coursedetails.coursename} 
+                        onChange={handleChange}/>
         </Form.Group>
         <Form.Group>
           <Form.Label>Enter course Details:</Form.Label>
           <Form.Control type="text" 
-                        placeholder="Enter course details" />
+                        placeholder="Enter course details"
+                        values={coursedetails.coursename} 
+                        onChange={handleChange} />
         </Form.Group>
         <Form.Group>
           <Form.Label>Enter course Details:</Form.Label>
--- Divya-Path_Frontend/src/components/CoursePage.js
+++ Divya-Path_Frontend/src/components/CoursePage.js
@@ -6,16 +6,18 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 
 export function CoursePage() {
+
     return (
+        
         <>
             <Navigation></Navigation>
             <div class="card">
                 <div class="card-header">
-                    Featured
+                  {coursedetails.coursename}
                 </div>
                 <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <h5 class="card-title"> as Special title treatment</h5>
+                    <p class="card-text">With supporting text belowa natural lead-in to additional content.</p>
                     <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
